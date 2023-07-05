e4366dc794914a9861144e67b7b8c7f81c2d587e
Angeth-Herjok <angethherjok@gmail.com>
2023-07-05 07:32:58 +0300

    first commit

---
+++ src/Features/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './style.css'
+
+const Features=()=>{
+    return(
+        <div>
+            <h1>Features of React in JavaScript</h1>
+            <p>JSX(JavaScript Syntax Extension)</p>
+            <p>Virtual DOM</p>
+            <p>One-way data binding</p>
+            <p>Performance</p>
+            <p>Extensions</p>
+            <p>Conditional Statements</p>
+            <p>Components</p>
+            <p>Simplicity</p>
+        </div>
+    )
+}
+export default Features;
